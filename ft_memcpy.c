fd0053e760585d58c509784aed96dfb454b35484
Lynn Haydar <lhaydar@c2r6s2.42beirut.com>
2025-11-01 18:48:00 +0200

    norminette memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,30 +6,29 @@
 /*   By: lhaydar <lhaydar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/01 18:26:53 by lhaydar           #+#    #+#             */
-/*   Updated: 2025/11/01 18:45:11 by lhaydar          ###   ########.fr       */
+/*   Updated: 2025/11/01 18:47:37 by lhaydar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 #include <unistd.h>
 
- void *ft_memcpy(void *dest, const void *src, size_t n)
- {
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
 	size_t			i;
 	unsigned char	*ptr;
 	unsigned char	*s;
 
 	ptr = (unsigned char *) dest;
 	s = (unsigned char *) src;
-	
 	i = 0;
-	while(i < n)
+	while (i < n)
 	{
 		ptr[i] = s[i];
 		i++;
 	}
 	return (ptr);
- }
+}
 
 //  int main(void)
 //  {
@@ -38,6 +37,5 @@
 // 	char *result = ft_memcpy(dest, src, 4);
 // 	for (int i = 0; i <6; i++)
 // 		write(1, &result[i], 1);
-// 	return 0 ;
-		
+// 	return 0 ;	
 //  }
