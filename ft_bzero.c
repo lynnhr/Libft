1e22868317856810d2c7dfb347d17563b761a605
Lynn Haydar <lhaydar@c2r6s2.42beirut.com>
2025-11-01 18:32:41 +0200

    bzero norminette

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,21 +6,21 @@
 /*   By: lhaydar <lhaydar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/01 17:13:09 by lhaydar           #+#    #+#             */
-/*   Updated: 2025/11/01 18:25:12 by lhaydar          ###   ########.fr       */
+/*   Updated: 2025/11/01 18:32:23 by lhaydar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 #include <unistd.h>
 
-void ft_bzero(void *s, size_t n)
+void	ft_bzero(void *s, size_t n)
 {
 	size_t			i;
-	unsigned char *ptr;
+	unsigned char	*ptr;
 
 	i = 0;
 	ptr = (unsigned char *) s;
-	while(i < n)
+	while (i < n)
 	{
 		ptr[i] = '\0';
 		i++;
@@ -28,11 +28,10 @@ void ft_bzero(void *s, size_t n)
 	s = ptr;
 }
 
-
-int main(void)
-{
-    char buffer[8];
-	ft_bzero(buffer, 6);
-	for(int i = 0; i< 6 ; i++)
-		write(1, &buffer[i], 1);
-}
+// int main(void)
+// {
+//     char buffer[8];
+// 	ft_bzero(buffer, 6);
+// 	for(int i = 0; i< 6 ; i++)
+// 		write(1, &buffer[i], 1);
+// }
