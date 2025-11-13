8f9d9b1e0a524d954496305751e98de2038b9001
Lynn Haydar <lhaydar@c2r6s4.42beirut.com>
2025-11-13 13:42:44 +0200

    isascii testing and norminette done

--- ft_isascii.c
+++ ft_isascii.c
@@ -6,7 +6,7 @@
 /*   By: lhaydar <lhaydar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/01 15:24:36 by lhaydar           #+#    #+#             */
-/*   Updated: 2025/11/13 13:08:35 by lhaydar          ###   ########.fr       */
+/*   Updated: 2025/11/13 13:42:08 by lhaydar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,3 +18,14 @@ int	ft_isascii(int c)
 		return (1);
 	return (0);
 }
+
+// #include <stdio.h>
+// int main(void)
+// {
+//     printf("ft_isascii('A') = %d\n", ft_isascii('A'));
+//     printf("ft_isascii('z') = %d\n", ft_isascii('z'));
+//     printf("ft_isascii('0') = %d\n", ft_isascii('0'));
+//     printf("ft_isascii(' ') = %d\n", ft_isascii(' '));
+// 	 printf("ft_isascii(' ') = %d\n", ft_isascii(128));
+//     return 0;
+// }
