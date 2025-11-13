2ed978dca61bf2a546bb0800e0528bb1d24a0eb1
Lynn Haydar <lhaydar@c2r6s4.42beirut.com>
2025-11-13 12:46:12 +0200

    alnum done

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -1,20 +1,34 @@
-/******************************************************************************/
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   ft_isalnum.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: lhaydar <lhaydar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/01 15:24:19 by lhaydar           #+#    #+#             */
-/*   Updated: 2025/11/13 11:31:26 by marvin           ###   ########.fr       */
+/*   Updated: 2025/11/13 12:39:57 by lhaydar          ###   ########.fr       */
 /*                                                                            */
-/******************************************************************************/
+/* ************************************************************************** */
 
 #include "libft.h"
 
 int	ft_isalnum(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= 0 && c <= 9))
-		return (1);
-	return (0);
+	return (ft_isdigit(c) || ft_isalpha(c));
+}
+
+#include <stdio.h>
+int main(void)
+{
+	char c1 = 'a';
+	char c2 = 'Z';
+	char c3 = '1';
+	char c4 = '%';
+
+	printf(" '%c' returns %d\n", c1, ft_isalnum(c1));
+	printf("'%c' returns %d\n", c2, ft_isalnum(c2));
+	printf("'%c' returns %d\n", c3, ft_isalnum(c3));
+	printf(" '%c' returns %d\n", c4, ft_isalnum(c4));
+
+	return 0;
 }
--- libft.h
+++ libft.h
@@ -1,14 +1,14 @@
-/******************************************************************************/
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: lhaydar <lhaydar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 13:34:40 by lhaydar           #+#    #+#             */
-/*   Updated: 2025/11/13 11:45:17 by marvin           ###   ########.fr       */
+/*   Updated: 2025/11/13 12:42:44 by lhaydar          ###   ########.fr       */
 /*                                                                            */
-/******************************************************************************/
+/* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
@@ -17,9 +17,10 @@
 #include <stdlib.h>
 #include <stddef.h>
 
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
 int	ft_isalnum(int c);
 int	ft_isascii(int c);	
-int	ft_isdigit(int c);
 int	ft_isprint(int c);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
@@ -33,5 +34,6 @@ int	ft_tolower(int c);
 int	ft_toupper(int c);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int	ft_atoi(const char *str);
+
 
 #endif
