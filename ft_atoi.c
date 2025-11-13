0fe0b476385cab7df21ed1b9e1490962bc024479
lynn <lynn@example.com>
2025-11-14 00:10:32 +0200

    atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -5,9 +5,43 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/13 11:39:51 by marvin            #+#    #+#             */
-/*   Updated: 2025/11/13 11:39:55 by marvin           ###   ########.fr       */
+/*   Created: 2025/11/14 00:10:03 by marvin            #+#    #+#             */
+/*   Updated: 2025/11/14 00:10:08 by marvin           ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
 #include "libft.h"
+
+int	ft_atoi(const char *nptr)
+{
+	int	i;
+	int	res;
+	int	sign;
+
+	i = 0;
+	res = 0;
+	sign = 1;
+	while (nptr[i] == 32 || (nptr[i] >= 9 && nptr[i] <= 13))
+		i++;
+	if (nptr[i] == '-' || nptr[i] == '+')
+	{
+		if (nptr[i == '-'])
+			sign = -1;
+		i++;
+	}
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		res = res * 10 + (nptr[i] - '0');
+		i++;
+	}
+	return (res * sign);
+}
+
+// #include <stdio.h>
+// int main()
+// {
+// 	char str[] = "   -1234abc";
+// 	int num = atoi(str);
+// 	printf("The converted integer is: %d\n", num);	
+// 	return 0;
+// }
--- libft.h
+++ libft.h
@@ -1,14 +1,14 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lhaydar <lhaydar@student.42.fr>            +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 13:34:40 by lhaydar           #+#    #+#             */
-/*   Updated: 2025/11/13 16:15:39 by lhaydar          ###   ########.fr       */
+/*   Updated: 2025/11/13 22:55:13 by marvin           ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
 #ifndef LIBFT_H
 # define LIBFT_H
@@ -29,13 +29,14 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void *ft_memmove(void *dest, const void *src, size_t n);
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size);
 unsigned	int	ft_strlcat(char *dest, char *src, unsigned int size);
+int ft_atoi(const char *nptr);
 
 void	*ft_memchr(const void *s, int c, size_t n);
 int	ft_strncmp(char *s1, char *s2, unsigned int n);
 int	ft_tolower(int c);
 int	ft_toupper(int c);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
-int	ft_atoi(const char *str);
+
 
 
 #endif
