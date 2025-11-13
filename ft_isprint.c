3fb79067de0bb3f356bebfc9ccc169980b15ef68
Lynn Haydar <lhaydar@c2r6s4.42beirut.com>
2025-11-13 13:55:13 +0200

    ft_isprint test & norm done

--- ft_isprint.c
+++ ft_isprint.c
@@ -1,14 +1,14 @@
-/******************************************************************************/
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   ft_isprint.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: lhaydar <lhaydar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/01 15:27:15 by lhaydar           #+#    #+#             */
-/*   Updated: 2025/11/13 11:31:58 by marvin           ###   ########.fr       */
+/*   Updated: 2025/11/13 13:54:28 by lhaydar          ###   ########.fr       */
 /*                                                                            */
-/******************************************************************************/
+/* ************************************************************************** */
 
 #include "libft.h"
 
@@ -18,3 +18,15 @@ int	ft_isprint(int c)
 		return (1);
 	return (0);
 }
+
+// #include <stdio.h>
+// int main(void)
+// {
+//     printf("ft_isprint('A') = %d\n", ft_isprint('A'));
+//     printf("ft_isprint(' ') = %d\n", ft_isprint(' '));
+//     printf("ft_isprint('~') = %d\n", ft_isprint('~'));
+//     printf("ft_isprint(31) = %d\n", ft_isprint(31));
+//     printf("ft_isprint(127) = %d\n", ft_isprint(127));
+//     printf("ft_isprint('\\n') = %d\n", ft_isprint('\n'));
+//     return 0;
+// }
