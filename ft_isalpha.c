3e9ab8d3b271aab4086f822f19eeed5b5350272d
Lynn Haydar <lhaydar@c2r6s4.42beirut.com>
2025-11-13 12:20:47 +0200

    testing isalpha correct

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -1,14 +1,14 @@
-/******************************************************************************/
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   ft_isalpha.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: lhaydar <lhaydar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 13:33:39 by lhaydar           #+#    #+#             */
-/*   Updated: 2025/11/13 12:00:15 by marvin           ###   ########.fr       */
+/*   Updated: 2025/11/13 12:14:24 by lhaydar          ###   ########.fr       */
 /*                                                                            */
-/******************************************************************************/
+/* ************************************************************************** */
 
 #include "libft.h"
 
@@ -20,21 +20,21 @@ int	ft_isalpha(int c)
 	return (0);
 }
 
-#include <stdio.h>
-int main(void)
-{
-	char c1 = 'a';
-	char c2 = 'Z';
-	char c3 = '1';
-	char c4 = '%';
+// #include <stdio.h>
+// int main(void)
+// {
+// 	char c1 = 'a';
+// 	char c2 = 'Z';
+// 	char c3 = '1';
+// 	char c4 = '%';
 
-	printf("Is '%c' an alphabetic character? %d\n", c1, ft_isalpha(testChar1));
-	printf("Is '%c' an alphabetic character? %d\n", c2, ft_isalpha(testChar2));
-	printf("Is '%c' an alphabetic character? %d\n", c3, ft_isalpha(testChar3));
-	printf("Is '%c' an alphabetic character? %d\n", c4, ft_isalpha(testChar4));
+// 	printf("is '%c' an alphabet? %d\n", c1, ft_isalpha(c1));
+// 	printf("is '%c' an alphabet? %d\n", c2, ft_isalpha(c2));
+// 	printf("is '%c' an alphabet? %d\n", c3, ft_isalpha(c3));
+// 	printf("is '%c' an alphabet? %d\n", c4, ft_isalpha(c4));
 
-	return 0;
-}
+// 	return 0;
+// }
 
 
 
