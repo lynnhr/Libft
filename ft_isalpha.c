78e34e54247d73400ad9039dfa865e87706ce664
lynn <lynn@example.com>
2025-11-13 12:06:50 +0200

    testing isalpha

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -1,21 +1,17 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   ft_isalpha.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lhaydar <lhaydar@student.42.fr>            +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 13:33:39 by lhaydar           #+#    #+#             */
-/*   Updated: 2025/11/01 15:29:44 by lhaydar          ###   ########.fr       */
+/*   Updated: 2025/11/13 12:00:15 by marvin           ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
 #include "libft.h"
 
-
-//implementing functio
-//testing pushing to github
-//testing pulling
 
 int	ft_isalpha(int c)
 {
@@ -24,6 +20,21 @@ int	ft_isalpha(int c)
 	return (0);
 }
 
+#include <stdio.h>
+int main(void)
+{
+	char c1 = 'a';
+	char c2 = 'Z';
+	char c3 = '1';
+	char c4 = '%';
+
+	printf("Is '%c' an alphabetic character? %d\n", c1, ft_isalpha(testChar1));
+	printf("Is '%c' an alphabetic character? %d\n", c2, ft_isalpha(testChar2));
+	printf("Is '%c' an alphabetic character? %d\n", c3, ft_isalpha(testChar3));
+	printf("Is '%c' an alphabetic character? %d\n", c4, ft_isalpha(testChar4));
+
+	return 0;
+}
 
 
 
--- libft.h
+++ libft.h
@@ -1,18 +1,37 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lhaydar <lhaydar@student.42.fr>            +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 13:34:40 by lhaydar           #+#    #+#             */
-/*   Updated: 2025/10/30 13:36:41 by lhaydar          ###   ########.fr       */
+/*   Updated: 2025/11/13 11:45:17 by marvin           ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-//decalre here the fuctions
+#include <unistd.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+int	ft_isalnum(int c);
+int	ft_isascii(int c);	
+int	ft_isdigit(int c);
+int	ft_isprint(int c);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memset(void *s, int c, size_t n);
+int	ft_strlen(char *str);
+unsigned	int	ft_strlcat(char *dest, char *src, unsigned int size);
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size);
+int	ft_strncmp(char *s1, char *s2, unsigned int n);
+int ft_isalpha(int c);
+int	ft_tolower(int c);
+int	ft_toupper(int c);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+int	ft_atoi(const char *str);
 
 #endif
