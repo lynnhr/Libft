ff7f88c43e1b73716a93d0cc096e5bdcabde9060
Lynn Haydar <lhaydar@c1r3s1.42beirut.com>
2025-11-14 12:48:09 +0200

    added prototypes

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lhaydar <lhaydar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 13:34:40 by lhaydar           #+#    #+#             */
-/*   Updated: 2025/11/14 11:47:31 by lhaydar          ###   ########.fr       */
+/*   Updated: 2025/11/14 12:47:09 by lhaydar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,5 +41,16 @@ char	*ft_strnstr(const char *big, const char *little, size_t len);
 int	ft_memcmp(const void *s1, const void *s2, size_t n);
 void *ft_calloc(size_t nmemb, size_t size);
 char *ft_strdup(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char *ft_strjoin(char const *s1, char const *s2);
+char *ft_strtrim(char const *s1, char const *set);
+char **ft_split(char const *s, char c);
+char *ft_itoa(int n);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+void ft_putchar_fd(char c, int fd);
+void ft_putstr_fd(char *s, int fd);
+void ft_putendl_fd(char *s, int fd);
+void ft_putnbr_fd(int n, int fd);
 
 #endif
